7f3589c5affa93e8e797be57c9911ac44c103e26
Tre Seibert <63426280+Tre-Seibert@users.noreply.github.com>
2023-12-04 13:25:05 -0500

    Created login.html for users to login and sign up.

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const port = 3000;
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve login.html when /login is accessed
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Serve landing.html for the root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'landing.html'));
 });
